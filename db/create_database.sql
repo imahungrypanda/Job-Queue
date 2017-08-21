9cafde24ff71b45ade21b0d67bd1cd7b7c962ea1
Steve Olsen <stevelolsen@gmail.com>
2017-08-21 14:54:54 -0700

    Create database if one doesn't exist

---
+++ db/create_database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE jobs (
+  id INTEGER PRIMARY KEY,
+  request_url VARCHAR(255) NOT NULL,
+  response_html TEXT
+);
--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express'),
-      app = express();
+      app = express(),
+      fs = require('fs'),
+      { exec } = require('child_process');
 
-const { PORT = 3000, NODE_ENV = 'development', DB = './db/databse.db'} = process.env;
+const { PORT = 3000, NODE_ENV = 'development', DB = './db/database.db'} = process.env;
 
 // Start Server
 Promise.resolve()
-  .then(() => app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`)))
+  // Create database
+  .then(() => {
+    if (!fs.existsSync(DB)) {
+      exec(`cat db/create_database.sql | sqlite3 ${DB}`, (error) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log('Database created');
+      });
+    }
+    else {
+      console.log('Database already exists');
+    }
+  })
+  .then(() => app.listen(PORT, () =>
+    console.log(`Server listening at http://localhost:${PORT}`)
+  ))
   .catch(err => {
     if (NODE_ENV === 'development') console.error(err.stack);
 });
@@ -14,3 +33,13 @@ Promise.resolve()
 app.get('/', (req, res) => {
   res.send('Return index of job queues?');
 });
+
+app.post('/', (req, res) =>{
+  console.log('create database entry');
+  console.log('start request to get HTML');
+  res.status(200).send({})
+});
+
+app.get('*', (req, res) =>
+  res.status(404).send({ message: 'Route does not exist' })
+);
